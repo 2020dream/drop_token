41b96c2c85526a5cd10c6c862ff9428ed76fad97
Zheng Cao <chori84@gmail.com>
2018-11-16 12:53:08 -0800

    Basic relation and validation tests for Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
     has_many :moves
     has_many :players
+
+    validates :columns, :numericality => { equal_to: 4 }
+    validates :rows, :numericality => { equal_to: 4 }
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
-class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Game do
+
+  describe 'relations' do
+    it "has two players" do
+      games(:one).players.length.must_equal 2
+    end
+  end
+
+  describe 'validations' do
+    let(:game) { games(:one) }
+
+    it "must be valid" do
+      game.valid?.must_equal true
+    end
+
+    it "is invalid with wrong value of columns" do
+      game.columns = 3
+      game.valid?.must_equal false
+    end
+
+    it "is invalid with wrong value of rows" do
+      game.rows = 3
+      game.valid?.must_equal false
+    end
+  end
+
 end
