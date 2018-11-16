246ac0d6bba31ca5f9c5a9213d68e7ac083456d7
Zheng Cao <chori84@gmail.com>
2018-11-16 13:07:13 -0800

    Config routes for GamesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'games/index'
-  get 'games/show'
-  get 'games/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games, only: [:index, :show, :create]
 end
