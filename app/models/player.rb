5ef8bb9e45919e1891577eef97da9f0897879127
Zheng Cao <chori84@gmail.com>
2018-11-16 12:42:32 -0800

    Basic relation and validation tests for Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
     has_many :moves
     belongs_to :game
+
+    validates :name, presence: true, uniqueness: true
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
-class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Player do
+
+  describe 'relations' do
+    it "belongs to a game" do
+      players(:one).game.must_equal games(:one)
+    end
+  end
+
+  describe 'validations' do
+    let(:player) { players(:one) }
+
+    it "must be valid" do
+      player.valid?.must_equal true
+    end
+
+    it "is invalid without name" do
+      player.name = nil
+      player.valid?.must_equal false
+    end
+  end
+
 end
