2b2f6dced0649beb043886abc3f531583d89a7f4
Zheng Cao <chori84@gmail.com>
2018-11-16 12:34:21 -0800

    Use Minitest as test framework

--- Gemfile
+++ Gemfile
@@ -43,3 +43,12 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :development, :test do
+  gem 'pry-rails'
+end
+
+group :test do
+  gem 'minitest-rails'
+  gem 'minitest-reporters'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/rails"
+require "minitest/reporters"
+
+# https://github.com/rails/rails/issues/31324
+if ActionPack::VERSION::STRING >= "5.2.0"
+  Minitest::Rails::TestUnit = Rails::TestUnit
+end
+
+#  For colorful output!
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
