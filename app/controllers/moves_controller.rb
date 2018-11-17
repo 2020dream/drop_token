f733f9c4eaa463d1dcd1cd6be6c00905551e2a23
Zheng Cao <chori84@gmail.com>
2018-11-16 20:33:39 -0800

    Implement show for MovesController

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -4,6 +4,13 @@ class MovesController < ApplicationController
   end
 
   def show
+    @move = Move.find_by(move_number: params[:id])
+
+    if @move.nil?
+      render json: { ok: false, :errors => "Move not found" }, status: :not_found 
+    else
+      render json: @move.as_json(only: [:type, :column], :include => { :player => { only: :name }}), status: :ok
+    end
   end
 
   def create
