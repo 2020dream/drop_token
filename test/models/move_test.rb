2853c2958602401cbdd44ca18c1bf56aace50813
Zheng Cao <chori84@gmail.com>
2018-11-17 15:16:32 -0800

    Model test for Move

--- test/models/move_test.rb
+++ test/models/move_test.rb
@@ -1,1 +1,38 @@
 require 'test_helper'
+
+describe Move do
+
+  describe 'relations' do
+    it "belongs to a game" do
+      moves(:one).game.id.must_equal games(:one).id
+    end
+
+    it "belongs to a player" do
+      moves(:one).player.id.must_equal players(:one).id
+    end
+  end
+
+  describe 'validations' do
+    let(:move) { moves(:one) }
+
+    it "must be valid" do
+      move.valid?.must_equal true
+    end
+
+    it "is invalid without column" do
+      move.column = nil
+      move.valid?.must_equal false
+    end
+
+    it "is invalid with wrong value of column" do
+      move.column = 4
+      move.valid?.must_equal false
+    end
+
+    it "is invalid with wrong type for column" do
+      move.column = "wrong_type"
+      move.valid?.must_equal false
+    end
+  end
+
+end
